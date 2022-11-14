e61aa57717548537f19e548896262e6bb2476dd5
KGCA4100 <KGCA4100@DESKTOP-AEAGHOI>
2022-11-14 18:50:37 +0900

    update

---
+++ okaka94/KJMCoreSample Test_Obj_Map_height/FBX_loader.cpp
@@ -0,0 +1,632 @@
+#include "FBX_loader.h"
+
+HRESULT	FBX_loader::CreateConstantBuffer(ID3D11Device* pDevice) {
+
+	HRESULT hr;
+	for (int Bone = 0; Bone < 255; Bone++) {
+		m_bone_cbData.Bone_mat[Bone].Set_I_matrix();
+	}
+	D3D11_BUFFER_DESC       bd;
+	ZeroMemory(&bd, sizeof(bd));
+	bd.ByteWidth = sizeof(VS_BONE_CONSTANT_BUFFER) * 1; // 바이트 용량
+	// GPU 메모리에 할당
+	bd.Usage = D3D11_USAGE_DEFAULT; // 버퍼의 할당 장소 내지는 버퍼용도
+	bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+
+	D3D11_SUBRESOURCE_DATA  sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.pSysMem = &m_bone_cbData;
+	hr = pDevice->CreateBuffer(
+		&bd, // 버퍼 할당
+		&sd, // 초기 할당된 버퍼를 체우는 CPU메모리 주소
+		&m_Bone_CB);
+
+
+	return hr;
+}
+
+bool		FBX_loader::Init() {
+
+	m_pFbxManager = FbxManager::Create();
+	m_pFbxImporter = FbxImporter::Create(m_pFbxManager, "");
+	m_pFbxScene = FbxScene::Create(m_pFbxManager, "");
+	return true;
+}
+
+void		FBX_loader::Update_anim(ID3D11DeviceContext* pContext) {
+
+		m_Anim_frame += g_fSecPerFrame *  m_Anim_scene.Frame_speed * m_Anim_inverse;
+		if (m_Anim_frame > m_Anim_scene.End_frame || m_Anim_frame < m_Anim_scene.Start_frame) {
+			m_Anim_frame = min(m_Anim_frame, m_Anim_scene.End_frame);
+			m_Anim_frame = max(m_Anim_frame, m_Anim_scene.Start_frame);
+			m_Anim_inverse *= -1.0f;
+		}
+
+		std::vector<Matrix> Current_anim_mat_list;
+		for (int Bone = 0; Bone < m_Obj_list.size(); Bone++) {
+			Matrix Anim_mat = m_Obj_list[Bone]->Interpolate(m_Anim_frame, m_Anim_scene);
+			m_bone_cbData.Bone_mat[Bone] = Anim_mat.Transpose();
+			Current_anim_mat_list.push_back(Anim_mat);
+		}
+		pContext->UpdateSubresource(m_Bone_CB, 0, nullptr, &m_bone_cbData, 0, 0);
+		
+		for (int draw = 0; draw < m_Draw_list.size(); draw++) {
+			if (m_Draw_list[draw]->m_Bindpose_mat_map.size()) {
+				for (int Bone = 0; Bone < m_Obj_list.size(); Bone++) {
+					auto iter = m_Draw_list[draw]->m_Bindpose_mat_map.find(Bone);
+					if (iter != m_Draw_list[draw]->m_Bindpose_mat_map.end()) {
+						Matrix Bind_mat = iter->second;
+						Matrix Anim_mat = Bind_mat * Current_anim_mat_list[Bone];
+						m_bone_cbData.Bone_mat[Bone] = Anim_mat.Transpose();
+					}
+				}
+				pContext->UpdateSubresource(m_Draw_list[draw]->m_Skin_Bone_CB, 0, nullptr, &m_bone_cbData, 0, 0);
+			}
+		}	
+}
+
+void		FBX_loader::Update_bone_data(ID3D11DeviceContext* pContext, float frame, VS_BONE_CONSTANT_BUFFER& cbData) {
+
+	for (int iBone = 0; iBone < m_Obj_list.size(); iBone++)
+	{
+		Matrix matAnimation = m_Obj_list[iBone]->Interpolate(frame, m_Anim_scene);
+		cbData.Bone_mat[iBone] = matAnimation;
+	}
+
+}
+
+void		FBX_loader::Update_sub_bone_data(ID3D11DeviceContext* pContext, VS_BONE_CONSTANT_BUFFER& cbInputData, std::vector< VS_BONE_CONSTANT_BUFFER>& cbOutputData) {
+
+	for (int iDraw = 0; iDraw < m_Draw_list.size(); iDraw++)
+	{
+		if (m_Draw_list[iDraw]->m_Bindpose_mat_map.size())
+		{
+			for (int Bone = 0; Bone < m_Obj_list.size(); Bone++)
+			{
+				auto iter = m_Draw_list[iDraw]->m_Bindpose_mat_map.find(Bone);
+				if (iter != m_Draw_list[iDraw]->m_Bindpose_mat_map.end())
+				{
+					Matrix Bind_mat = iter->second;
+					Matrix Anim_mat = Bind_mat * cbInputData.Bone_mat[Bone];
+					cbOutputData[iDraw].Bone_mat[Bone] = Anim_mat.Transpose();
+				}
+			}
+		}
+	}
+}
+
+
+
+bool		FBX_loader::Render() {
+	for (auto obj : m_Draw_list) {
+		obj->Render();
+	}
+	return true;
+}
+
+bool		FBX_loader::Release() {
+
+	if (m_Bone_CB) {
+		m_Bone_CB->Release();
+		m_Bone_CB = nullptr;
+	}
+
+	for (auto obj : m_Obj_list) {
+	//for (auto obj : m_Draw_list) {
+		obj->Release();
+		delete obj;
+	}
+
+	m_Obj_list.clear();
+	
+	m_pFbxScene->Destroy();
+	if (m_pFbxImporter != nullptr)
+	{
+		m_pFbxImporter->Destroy();
+		m_pFbxImporter = nullptr;
+	}
+	if (m_pFbxManager != nullptr)
+	{
+		m_pFbxManager->Destroy();
+		m_pFbxManager = nullptr;
+	}
+
+	return true;
+}
+
+bool		FBX_loader::Load(C_STR filename) {
+
+	m_pFbxImporter->Initialize(filename.c_str());
+	m_pFbxImporter->Import(m_pFbxScene);
+	FbxSystemUnit::m.ConvertScene(m_pFbxScene);				// meter
+	FbxAxisSystem::MayaZUp.ConvertScene(m_pFbxScene);		// 기저축
+
+	Init_animation();
+
+	m_pRootNode = m_pFbxScene->GetRootNode();
+	Pre_Process(m_pRootNode);
+
+	for (auto obj : m_Obj_list) {
+		if (obj->m_pFbxParentNode != nullptr) {
+			auto data = m_Obj_map.find(obj->m_pFbxParentNode);
+			obj->Set_parent(data->second);
+		}
+		
+		FbxMesh* mesh = obj->m_pFbxNode->GetMesh();
+		if (mesh) {
+			
+			Parse_mesh(mesh, obj);
+		}
+	}
+
+	FbxTime Time;
+	for (FbxLongLong Frame = m_Anim_scene.Start_frame; Frame <= m_Anim_scene.End_frame; Frame++) {
+		Time.SetFrame(Frame, m_Anim_scene.Time_mode);
+		Load_animation(Frame, Time);
+	}
+
+	return true;
+}
+
+void		FBX_loader::Pre_Process(FbxNode* node) {
+
+	if (node && (node->GetCamera() || node->GetLight())) return; // node가 null이 아니고 , camera or light 데이터라면 사용 X
+
+	Skinning_FBX_obj* obj = new Skinning_FBX_obj;
+	std::string name = node->GetName();	// node의 이름 저장
+	obj->m_obj_name = to_mw(name);
+	obj->m_pFbxNode = node;
+	obj->m_pFbxParentNode = node->GetParent();
+	obj->m_Object_Bone = m_Obj_list.size();
+
+	m_Obj_list.push_back(obj);
+	m_Obj_map.insert(std::make_pair(node, obj));
+	m_Obj_ID_map.insert(std::make_pair(node, obj->m_Object_Bone));
+
+	int child_num = node->GetChildCount();
+	for (int n = 0; n < child_num; n++) {
+		FbxNode* child_node = node->GetChild(n);   // n번째 child_node를 Get
+		FbxNodeAttribute::EType type = child_node->GetNodeAttribute()->GetAttributeType();
+		if (type == FbxNodeAttribute::eMesh ||
+			type == FbxNodeAttribute::eSkeleton ||
+			type == FbxNodeAttribute::eNull) {
+			Pre_Process(child_node);
+		}
+	}
+}
+bool	FBX_loader::Parse_mesh_skinning(FbxMesh* mesh, Skinning_FBX_obj* obj) {
+
+	int Deformer_cnt = mesh->GetDeformerCount(FbxDeformer::eSkin);
+	if (Deformer_cnt == 0) return false;
+
+	int Vertex_num = mesh->GetControlPointsCount();
+	obj->m_Weight_list.resize(Vertex_num);
+
+	for (int Deformer = 0; Deformer < Deformer_cnt; Deformer++) {
+		FbxDeformer* fbx_deformer = mesh->GetDeformer(Deformer, FbxDeformer::eSkin);
+		FbxSkin* Skin = (FbxSkin*)fbx_deformer;
+
+		int Cluster_cnt = Skin->GetClusterCount();
+		for (int Cluster = 0; Cluster < Cluster_cnt; Cluster++) {
+			FbxCluster* fbx_cluster = Skin->GetCluster(Cluster);
+			FbxNode* node = fbx_cluster->GetLink();
+			int Bone_idx = m_Obj_ID_map.find(node)->second;
+
+			FbxAMatrix XBind_pos_mat;
+			FbxAMatrix Global_init_pos;
+			fbx_cluster->GetTransformLinkMatrix(XBind_pos_mat);
+			fbx_cluster->GetTransformMatrix(Global_init_pos);
+			FbxAMatrix Bind_pos_mat = Global_init_pos.Inverse() * XBind_pos_mat;
+
+			Matrix Inv_Bind_pos_mat = Convert_DX_mat(Bind_pos_mat);
+			Inv_Bind_pos_mat = Inv_Bind_pos_mat.Inverse();
+			obj->m_Bindpose_mat_map.insert(std::make_pair(Bone_idx, Inv_Bind_pos_mat));
+
+			int Weight_cnt = fbx_cluster->GetControlPointIndicesCount();
+			int* Indices = fbx_cluster->GetControlPointIndices();
+			double* Weights = fbx_cluster->GetControlPointWeights();
+			for (int vertex = 0; vertex < Weight_cnt; vertex++) {
+				int index = Indices[vertex];
+				float weight = Weights[vertex];
+				obj->m_Weight_list[index].Insert(Bone_idx, weight);
+			}
+		}
+	}
+
+	return true;
+}
+
+void		FBX_loader::Parse_mesh(FbxMesh* mesh, Skinning_FBX_obj* obj) {
+
+	obj->m_skinned = Parse_mesh_skinning(mesh, obj);
+
+	FbxNode* node = mesh->GetNode();
+	FbxAMatrix geom;	// 기하 행렬
+	FbxVector4 T = node->GetGeometricTranslation(FbxNode::eSourcePivot);
+	FbxVector4 R = node->GetGeometricRotation(FbxNode::eSourcePivot);
+	FbxVector4 S = node->GetGeometricScaling(FbxNode::eSourcePivot);
+	geom.SetT(T);
+	geom.SetR(R);
+	geom.SetS(S);
+	FbxAMatrix normalLocalMatrix = geom;
+
+	normalLocalMatrix = normalLocalMatrix.Inverse();
+	normalLocalMatrix = normalLocalMatrix.Transpose();
+
+	FbxLayerElementUV* UV_set = nullptr;
+	FbxLayerElementVertexColor* Color_set = nullptr;
+	FbxLayerElementNormal* Normal_set = nullptr;
+	FbxLayerElementMaterial* Material_set = nullptr;
+	FbxLayer* Layer = mesh->GetLayer(0);
+
+	if (Layer->GetUVs() != nullptr) {
+		UV_set = Layer->GetUVs();
+	}
+	if (Layer->GetVertexColors() != nullptr) {
+		Color_set = Layer->GetVertexColors();
+	}
+	if (Layer->GetNormals() != nullptr) {
+		Normal_set = Layer->GetNormals();
+	}
+	if (Layer->GetMaterials() != nullptr) {
+		Material_set = Layer->GetMaterials();
+	}
+
+	std::string filename;
+	int material_num = node->GetMaterialCount();
+	std::vector<C_STR> Tex_name_list;
+	Tex_name_list.resize(material_num);
+
+	for (int idx = 0; idx < material_num; idx++) {
+		FbxSurfaceMaterial* surface = node->GetMaterial(idx);
+		if (surface) {
+			auto property = surface->FindProperty(FbxSurfaceMaterial::sDiffuse);
+			if (property.IsValid()) {
+				const FbxFileTexture* tex = property.GetSrcObject<FbxFileTexture>(0);
+				if (tex) {
+					filename = tex->GetFileName();
+					Tex_name_list[idx] = filename;
+				}
+			}
+		}
+	}
+
+	if (material_num == 1) {
+		obj->m_Texture_name = Texture_manager::GetInstance().Get_split_name(filename);
+	}
+	if (material_num > 1) {
+		obj->m_VBdata_list.resize(material_num);
+		obj->m_VBdata_list_IW.resize(material_num);
+		obj->m_SubTEX_name_list.resize(material_num);
+		for (int idx = 0; idx < material_num; idx++) {
+			obj->m_SubTEX_name_list[idx] = Texture_manager::GetInstance().Get_split_name(Tex_name_list[idx]);
+		}
+	}
+
+	int poly_cnt = mesh->GetPolygonCount();
+	int face_num = 0;
+	int base_poly_idx = 0;
+	int sub_material = 0;
+
+	FbxVector4* Vertex_pos = mesh->GetControlPoints();
+	for (int poly = 0; poly < poly_cnt; poly++) {
+		int poly_size = mesh->GetPolygonSize(poly);
+		face_num = poly_size - 2; // 폴리곤 정점의 수 - 2 하면 폴리곤 구성이 3정점, 4정점 무관하게 페이스의 수 구할 수 있음
+		if (Material_set) {
+			sub_material = Get_SubMaterial_index(poly, Material_set);
+		}
+		for (int face = 0; face < face_num; face++) {			// 3정점 , 4정점 무관하게 돌릴 수 있도록 보정하는 것
+			// 정점 컬러 인덱스
+			int vertex_color[3] = { 0,face + 2,face + 1 };
+			// 정점 인덱스
+			int corner_index[3];
+			corner_index[0] = mesh->GetPolygonVertex(poly, 0);
+			corner_index[1] = mesh->GetPolygonVertex(poly, face+2);
+			corner_index[2] = mesh->GetPolygonVertex(poly, face+1);
+			// UV 인덱스
+			int uv_index[3];
+			uv_index[0] = mesh->GetTextureUVIndex(poly, 0);
+			uv_index[1] = mesh->GetTextureUVIndex(poly, face+2);
+			uv_index[2] = mesh->GetTextureUVIndex(poly, face+1);
+
+			for (int idx = 0; idx < 3; idx++) {
+				int vertex_index = corner_index[idx];
+				FbxVector4 v2 = Vertex_pos[vertex_index];
+				PNCTVertex vertex_data;
+				IW_VERTEX IW_vertex_data;
+				FbxVector4 v = geom.MultT(v2);
+				vertex_data.p.x = v.mData[0];
+				vertex_data.p.y = v.mData[2];
+				vertex_data.p.z = v.mData[1];
+				vertex_data.c = Vector4D(1, 1, 1, 1);
+				if (Color_set) {
+					FbxColor C = Read_color(mesh, Color_set, corner_index[idx], base_poly_idx + vertex_color[idx]);
+					vertex_data.c.x = C.mRed;
+					vertex_data.c.y = C.mGreen;
+					vertex_data.c.z = C.mBlue;
+					vertex_data.c.w = 1.0f;
+				}
+				if (UV_set) {
+					FbxVector2 T = Read_tex_coord(mesh, UV_set, corner_index[idx], uv_index[idx]);
+					vertex_data.t.x = T.mData[0];
+					vertex_data.t.y = 1.0f - T.mData[1];
+				}
+				if (Normal_set) {
+					FbxVector4 N = Read_normal(mesh, Normal_set, corner_index[idx], base_poly_idx + vertex_color[idx]);
+					N = normalLocalMatrix.MultT(N);
+					vertex_data.n.x = N.mData[0];
+					vertex_data.n.y = N.mData[2];
+					vertex_data.n.z = N.mData[1];
+				}
+				if (obj->m_skinned == false) {
+					IW_vertex_data.i.x = m_Obj_ID_map.find(node)->second;
+					IW_vertex_data.i.y = 0;
+					IW_vertex_data.i.z = 0;
+					IW_vertex_data.i.w = 0;
+					IW_vertex_data.w.x = 1.0f;
+					IW_vertex_data.w.y = 0.0f;
+					IW_vertex_data.w.z = 0.0f;
+					IW_vertex_data.w.w = 0.0f;
+				}
+				else {
+					Weight* pWeight = &obj->m_Weight_list[vertex_index];
+					IW_vertex_data.i.x = pWeight->Index[0];
+					IW_vertex_data.i.y = pWeight->Index[1];
+					IW_vertex_data.i.z = pWeight->Index[2];
+					IW_vertex_data.i.w = pWeight->Index[3];
+					IW_vertex_data.w.x = pWeight->weight[0];
+					IW_vertex_data.w.y = pWeight->weight[1];
+					IW_vertex_data.w.z = pWeight->weight[2];
+					IW_vertex_data.w.w = pWeight->weight[3];
+				}
+				if (material_num <= 1) {
+					obj->m_VertexList.push_back(vertex_data);
+					obj->m_Vertex_list_IW.push_back(IW_vertex_data);
+				}
+				else {
+					obj->m_VBdata_list[sub_material].push_back(vertex_data);
+					obj->m_VBdata_list_IW[sub_material].push_back(IW_vertex_data);
+				}
+			}
+		}
+		base_poly_idx += poly_size;
+	}
+	m_Draw_list.push_back(obj);
+}
+
+FbxVector2	FBX_loader::Read_tex_coord(FbxMesh* mesh, FbxLayerElementUV* UV_set, int pos_indx, int data_indx) {
+
+	FbxVector2 t;
+	FbxLayerElement::EMappingMode mode = UV_set->GetMappingMode();
+	switch (mode)
+	{
+	case FbxLayerElementUV::eByControlPoint:
+	{
+		switch (UV_set->GetReferenceMode())
+		{
+		case FbxLayerElementUV::eDirect:
+		{
+			t = UV_set->GetDirectArray().GetAt(pos_indx);
+		}break;
+		case FbxLayerElementUV::eIndexToDirect:
+		{
+			int index = UV_set->GetIndexArray().GetAt(pos_indx);
+			t = UV_set->GetDirectArray().GetAt(index);
+		}break;
+		}break;
+	} break;
+	case FbxLayerElementUV::eByPolygonVertex:
+	{
+		switch (UV_set->GetReferenceMode())
+		{
+		case FbxLayerElementUV::eDirect:
+		case FbxLayerElementUV::eIndexToDirect:
+		{
+			t = UV_set->GetDirectArray().GetAt(data_indx);
+		}break;
+		}break;
+	}break;
+	}
+
+	return t;
+}
+
+FbxColor	FBX_loader::Read_color(FbxMesh* mesh, FbxLayerElementVertexColor* Color_set, int pos_indx, int data_indx) {
+	FbxColor color(1, 1, 1, 1);
+	FbxLayerElement::EMappingMode mode = Color_set->GetMappingMode();
+	switch (mode)
+	{
+	case FbxLayerElementUV::eByControlPoint:
+	{
+		switch (Color_set->GetReferenceMode())
+		{
+		case FbxLayerElementUV::eDirect:
+		{
+			color = Color_set->GetDirectArray().GetAt(pos_indx);
+		}break;
+		case FbxLayerElementUV::eIndexToDirect:
+		{
+			int index = Color_set->GetIndexArray().GetAt(pos_indx);
+			color = Color_set->GetDirectArray().GetAt(index);
+		}break;
+		}break;
+	} break;
+	case FbxLayerElementUV::eByPolygonVertex:
+	{
+		switch (Color_set->GetReferenceMode())
+		{
+		case FbxLayerElementUV::eDirect:
+		{
+			color = Color_set->GetDirectArray().GetAt(data_indx);
+		}break;
+		case FbxLayerElementUV::eIndexToDirect:
+		{
+			int index = Color_set->GetIndexArray().GetAt(data_indx);
+			color = Color_set->GetDirectArray().GetAt(index);
+		}break;
+		}break;
+	}break;
+	}
+	return color;
+
+
+}
+FbxVector4	FBX_loader::Read_normal(FbxMesh* mesh, FbxLayerElementNormal* Normal_set, int pos_indx, int data_indx) {
+
+	FbxVector4 normal(1, 1, 1, 1);
+	FbxLayerElement::EMappingMode mode = Normal_set->GetMappingMode();
+	switch (mode)
+	{
+	case FbxLayerElementUV::eByControlPoint:
+	{
+		switch (Normal_set->GetReferenceMode())
+		{
+		case FbxLayerElementUV::eDirect:
+		{
+			normal = Normal_set->GetDirectArray().GetAt(pos_indx);
+		}break;
+		case FbxLayerElementUV::eIndexToDirect:
+		{
+			int index = Normal_set->GetIndexArray().GetAt(pos_indx);
+			normal = Normal_set->GetDirectArray().GetAt(index);
+		}break;
+		}break;
+	} break;
+	case FbxLayerElementUV::eByPolygonVertex:
+	{
+		switch (Normal_set->GetReferenceMode())
+		{
+		case FbxLayerElementUV::eDirect:
+		{
+			normal = Normal_set->GetDirectArray().GetAt(data_indx);
+		}break;
+		case FbxLayerElementUV::eIndexToDirect:
+		{
+			int index = Normal_set->GetIndexArray().GetAt(data_indx);
+			normal = Normal_set->GetDirectArray().GetAt(index);
+		}break;
+		}break;
+	}break;
+	}
+	return normal;
+
+
+}
+
+int			FBX_loader::Get_SubMaterial_index(int poly, FbxLayerElementMaterial* Material_set) {
+
+	int iSubMtrl = 0;
+	if (Material_set != nullptr)
+	{
+		switch (Material_set->GetMappingMode())
+		{
+		case FbxLayerElement::eByPolygon:
+		{
+			// 매핑 정보가 배열에 저장되는 방식
+			switch (Material_set->GetReferenceMode())
+			{
+			case FbxLayerElement::eIndex:
+			{
+				iSubMtrl = poly;
+			}break;
+			case FbxLayerElement::eIndexToDirect:
+			{
+				iSubMtrl = Material_set->GetIndexArray().GetAt(poly);
+			}break;
+			}
+		}
+		default:
+		{
+			break;
+		}
+		}
+	}
+	return iSubMtrl;
+
+}
+
+void		FBX_loader::Init_animation() {
+
+	//the highest level container for animation data. An animation stack is equivalent to one take of animation.
+	FbxAnimStack* Anim_stack = m_pFbxScene->GetSrcObject<FbxAnimStack>(0);
+	FbxLongLong Start_frame = 0;
+	FbxLongLong End_frame = 0;
+	FbxTime::EMode Time_mode;
+	if (Anim_stack) {
+		FbxString Take_name = Anim_stack->GetName();
+		FbxTakeInfo* Take = m_pFbxScene->GetTakeInfo(Take_name);
+		FbxTime::SetGlobalTimeMode(FbxTime::eFrames30);
+		Time_mode = FbxTime::GetGlobalTimeMode();
+		FbxTimeSpan Lcl_time_span = Take->mLocalTimeSpan;			//TimeSpan 생성자는 Start Time , Stop Time
+		FbxTime	Start = Lcl_time_span.GetStart();					// Animation Take의 TaimeSpan을 통해 Start time과 Stop time 받아오기
+		FbxTime	End = Lcl_time_span.GetStop();
+		// duration
+		FbxTime Duration = Lcl_time_span.GetDuration();
+		Start_frame = Start.GetFrameCount(Time_mode);				// Anim Take의 Time정보를 이전에 Global Set한 Time_mode에 맞는 프레임단위로 변환하여 저장
+		End_frame = End.GetFrameCount(Time_mode);
+	}
+	m_Anim_scene.Start_frame = Start_frame;					// 받아온 Take의 정보를 해당 오브젝트의 Anim_scene 구조체에 입력
+	m_Anim_scene.End_frame = End_frame;
+	m_Anim_scene.Frame_speed = 30.0f;
+	m_Anim_scene.TickPerFrame = 160;
+	m_Anim_scene.Time_mode = Time_mode;
+
+}
+
+void	FBX_loader::Load_animation(FbxLongLong Frame, FbxTime Time) {
+		
+	for (auto obj : m_Obj_list) {
+
+		FbxNode* node = obj->m_pFbxNode;
+		Anim_track Track;
+		Track.frame = Frame;
+		FbxAMatrix fbx_mat = node->EvaluateGlobalTransform(Time);	//EvaluateGlobalTransform의 인자는 Time이므로 프레임별 Time을 다시 구해서 해당 프레임의 Time값을 넘겨 행렬을 받아오면 됨
+		Track.Anim_matrix = Convert_DX_mat(fbx_mat);
+		Track.Anim_matrix.Decompose(Track.S, Track.R, Track.T);
+		obj->m_Anim_track_list.push_back(Track);
+	}
+}
+//TBASIS_EX::TMatrix	FBX_loader::Convert_mat(FbxAMatrix& fbx_mat) {
+//	TBASIS_EX::TMatrix mat;
+//	float* arr = (float*)(&mat);
+//	double* fbx_arr = (double*)(&fbx_mat);
+//	for (int i = 0; i < 16; i++) {
+//		arr[i] = fbx_arr[i];
+//	}
+//	return mat;
+//}
+//
+//TBASIS_EX::TMatrix	FBX_loader::Convert_DX_mat(FbxAMatrix& fbx_mat) {
+//	TBASIS_EX::TMatrix converted_fbx_mat = Convert_mat(fbx_mat);
+//	TBASIS_EX::TMatrix mat;
+//
+//	mat._11 = converted_fbx_mat._11; mat._12 = converted_fbx_mat._13; mat._13 = converted_fbx_mat._12;
+//	mat._21 = converted_fbx_mat._31; mat._22 = converted_fbx_mat._33; mat._23 = converted_fbx_mat._32;
+//	mat._31 = converted_fbx_mat._21; mat._32 = converted_fbx_mat._23; mat._33 = converted_fbx_mat._22;
+//	mat._41 = converted_fbx_mat._41; mat._42 = converted_fbx_mat._43; mat._43 = converted_fbx_mat._42;
+//	mat._14 = mat._24 = mat._34 = 0.0f;
+//	mat._44 = 1.0f;
+//	return mat;
+//}
+Matrix	FBX_loader::Convert_mat(FbxAMatrix& fbx_mat) {
+	Matrix mat;
+	float* arr = (float*)(&mat);
+	double* fbx_arr = (double*)(&fbx_mat);
+	for (int i = 0; i < 16; i++) {
+		arr[i] = fbx_arr[i];
+	}
+	return mat;
+}
+
+	Matrix	FBX_loader::Convert_DX_mat(FbxAMatrix& fbx_mat) {
+	Matrix converted_fbx_mat = Convert_mat(fbx_mat);
+	Matrix mat;
+
+	mat._11 = converted_fbx_mat._11; mat._12 = converted_fbx_mat._13; mat._13 = converted_fbx_mat._12;
+	mat._21 = converted_fbx_mat._31; mat._22 = converted_fbx_mat._33; mat._23 = converted_fbx_mat._32;
+	mat._31 = converted_fbx_mat._21; mat._32 = converted_fbx_mat._23; mat._33 = converted_fbx_mat._22;
+	mat._41 = converted_fbx_mat._41; mat._42 = converted_fbx_mat._43; mat._43 = converted_fbx_mat._42;
+	mat._14 = mat._24 = mat._34 = 0.0f;
+	mat._44 = 1.0f;
+	return mat;
+}
---
+++ okaka94/KJMCoreSample Test_Obj_Map_height/Sample.cpp
@@ -0,0 +1,237 @@
+#include "Sample.h"
+void Sample::ClearD3D11DeviceContext(ID3D11DeviceContext* pd3dDeviceContext)
+{
+	// Unbind all objects from the immediate context
+	if (pd3dDeviceContext == NULL) return;
+
+	ID3D11ShaderResourceView* pSRVs[16] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+	ID3D11RenderTargetView* pRTVs[16] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+	ID3D11DepthStencilView* pDSV = NULL;
+	ID3D11Buffer* pBuffers[16] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+	ID3D11SamplerState* pSamplers[16] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+	UINT StrideOffset[16] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+
+	// Shaders
+	//pd3dDeviceContext->VSSetShader(NULL, NULL, 0);
+	//pd3dDeviceContext->HSSetShader(NULL, NULL, 0);
+	//pd3dDeviceContext->DSSetShader(NULL, NULL, 0);
+	//pd3dDeviceContext->GSSetShader(NULL, NULL, 0);
+	//pd3dDeviceContext->PSSetShader(NULL, NULL, 0);
+
+	//// IA clear
+	//pd3dDeviceContext->IASetVertexBuffers(0, 16, pBuffers, StrideOffset, StrideOffset);
+	//pd3dDeviceContext->IASetIndexBuffer(NULL, DXGI_FORMAT_R16_UINT, 0);
+	//pd3dDeviceContext->IASetInputLayout(NULL);
+
+	//// Constant buffers
+	//pd3dDeviceContext->VSSetConstantBuffers(0, 14, pBuffers);
+	//pd3dDeviceContext->HSSetConstantBuffers(0, 14, pBuffers);
+	//pd3dDeviceContext->DSSetConstantBuffers(0, 14, pBuffers);
+	//pd3dDeviceContext->GSSetConstantBuffers(0, 14, pBuffers);
+	//pd3dDeviceContext->PSSetConstantBuffers(0, 14, pBuffers);
+
+	// Resources
+	pd3dDeviceContext->VSSetShaderResources(0, 16, pSRVs);
+	//pd3dDeviceContext->HSSetShaderResources(0, 16, pSRVs);
+	//pd3dDeviceContext->DSSetShaderResources(0, 16, pSRVs);
+	//pd3dDeviceContext->GSSetShaderResources(0, 16, pSRVs);
+	pd3dDeviceContext->PSSetShaderResources(0, 16, pSRVs);
+
+	//// Samplers
+	//pd3dDeviceContext->VSSetSamplers(0, 16, pSamplers);
+	//pd3dDeviceContext->HSSetSamplers(0, 16, pSamplers);
+	//pd3dDeviceContext->DSSetSamplers(0, 16, pSamplers);
+	//pd3dDeviceContext->GSSetSamplers(0, 16, pSamplers);
+	//pd3dDeviceContext->PSSetSamplers(0, 16, pSamplers);
+
+	//// Render targets
+	//pd3dDeviceContext->OMSetRenderTargets(8, pRTVs, pDSV);
+
+	//// States
+	//FLOAT blendFactor[4] = { 0,0,0,0 };
+	//pd3dDeviceContext->OMSetBlendState(NULL, blendFactor, 0xFFFFFFFF);
+	//pd3dDeviceContext->OMSetDepthStencilState(NULL, 0);
+	//pd3dDeviceContext->RSSetState(NULL);
+}
+
+bool Sample::Init()
+{
+	
+	UINT fbx_idx = 0;
+
+	//FBX_loader* Man = new FBX_loader;
+	//if (Man->Init())
+	//{
+	//	if (Man->Load("../../data/fbx/Man.FBX")) {
+	//		Man->CreateConstantBuffer(m_pd3dDevice.Get());
+	//	}
+
+	//}
+	//m_fbx_table.insert(std::make_pair(L"Man", fbx_idx++));
+	//m_fbx_list.push_back(Man);
+
+	FBX_loader* Swat_turn = new FBX_loader;
+	if (Swat_turn->Init())
+	{
+		if (Swat_turn->Load("../../data/fbx/Swat@turning_right_45_degrees.fbx"))
+		{
+			Swat_turn->CreateConstantBuffer(m_pd3dDevice.Get());
+		}
+	}
+	m_fbx_table.insert(std::make_pair(L"Swat_turn", fbx_idx++));
+	m_fbx_list.push_back(Swat_turn);
+
+	FBX_loader* Swat = new FBX_loader;
+	if (Swat->Init())
+	{
+		Swat->Load("../../data/fbx/Swat.fbx");
+	}
+	m_fbx_table.insert(std::make_pair(L"Swat", fbx_idx++));
+	m_fbx_list.push_back(Swat);
+
+
+	W_STR szDefaultDir = L"../../data/fbx/";
+	std::wstring shaderfilename = L"Skinning.txt";
+
+	for (auto fbx : m_fbx_list)
+	{
+		for (int iObj = 0; iObj < fbx->m_Draw_list.size(); iObj++)
+		{
+			FBX_obj* pObj = fbx->m_Draw_list[iObj];
+			std::wstring  szLoad = szDefaultDir + pObj->m_Texture_name;
+			pObj->Create(m_pd3dDevice.Get(), m_pImmediateContext.Get(), shaderfilename, szLoad);
+		}
+	}
+	UINT FBX_INDEX = 0;
+
+
+	User_char = new FBX_char;
+	FBX_INDEX = m_fbx_table.find(L"Swat")->second;
+	User_char->m_FBX_loader = m_fbx_list[FBX_INDEX];
+	FBX_INDEX = m_fbx_table.find(L"Swat_turn")->second;
+	User_char->m_FBX_action = m_fbx_list[FBX_INDEX];
+	User_char->CreateConstantBuffer(m_pd3dDevice.Get());
+
+	if (User_char->m_FBX_action)
+	{
+		User_char->m_Anim_scene = User_char->m_FBX_action->m_Anim_scene;
+		User_char->m_FBX_action_list.insert(std::make_pair(L"walking", User_char->m_FBX_action));
+		User_char->m_Current_action.Start_frame = User_char->m_FBX_action->m_Anim_scene.Start_frame;
+		User_char->m_Current_action.End_frame = 50;// pFbxLoaderA->m_AnimScene.iEndFrame;
+	}
+	else
+	{
+		User_char->m_Anim_scene = User_char->m_FBX_loader->m_Anim_scene;
+		Action_table action;
+		action.Start_frame = User_char->m_Anim_scene.Start_frame;
+		action.End_frame = User_char->m_Anim_scene.End_frame;
+		action.Loop_state = true;
+		User_char->m_Action_map.insert(std::make_pair(L"idle", action));
+	}
+
+
+	//for (int iObj = 0; iObj < 5; iObj++)
+	//{
+	//	FBX_char* pNpc = new FBX_char;
+	//	
+	//	FBX_INDEX = m_fbx_table.find(L"Man")->second;
+	//	pNpc->m_FBX_loader = m_fbx_list[FBX_INDEX];
+	//	pNpc->m_World_matrix._41 = -4.0f + iObj * 2;
+	//	pNpc->m_Anim_scene = pNpc->m_FBX_loader->m_Anim_scene;
+	//	pNpc->CreateConstantBuffer(m_pd3dDevice.Get());
+	//	Action_table action;
+	//	action.Start_frame = 61;
+	//	action.End_frame = 91;
+	//	action.Loop_state = true;
+	//	pNpc->m_Action_map.insert(std::make_pair(L"walk", action));
+	//	pNpc->m_Current_action = pNpc->m_Action_map.find(L"walk")->second;
+
+	//	m_NpcList.push_back(pNpc);
+	//}
+
+
+
+	Main_cam = new Camera_debug;
+	Main_cam->Create_View_matrix(Vector(0, 1, -20), Vector(0, 0, 0), Vector(0, 1, 0));
+	Main_cam->Create_Proj_matrix(1.0f, 10000.0f, PI * 0.25f, (float)g_rtClient.right / (float)g_rtClient.bottom);
+
+
+	BG = new Map;
+	BG->Load_height_map(m_pd3dDevice.Get(), m_pImmediateContext.Get(), L"../../data/map/heightMap513.bmp");
+	BG->Build(m_pd3dDevice.Get(), BG->cols_num, BG->rows_num, Main_cam);
+	BG->Create(m_pd3dDevice.Get(), m_pImmediateContext.Get(), L"DefaultShape_PNCT.txt", L"../../data/NormalMap/stone_wall.bmp");
+	BG->Create_Qtree(m_pd3dDevice.Get(), Main_cam);
+
+	
+	return true;
+}
+bool Sample::Frame()
+{
+	ClearD3D11DeviceContext(m_pImmediateContext.Get());
+	Main_cam->Frame();
+
+	/*for (auto npc : m_NpcList)
+	{
+		npc->Update_anim(m_pImmediateContext.Get());
+	}*/
+
+	User_char->Update_anim(m_pImmediateContext.Get());
+
+	return true;
+}
+bool Sample::Render()
+{
+
+	/*for (int iNpc = 0; iNpc < m_NpcList.size(); iNpc++)
+	{
+		m_NpcList[iNpc]->SetMatrix(nullptr, &Main_cam->m_View_matrix, &Main_cam->m_Proj_matrix);
+		m_NpcList[iNpc]->Render(m_pImmediateContext.Get());
+	}*/
+
+	User_char->SetMatrix(nullptr, &Main_cam->m_View_matrix, &Main_cam->m_Proj_matrix);
+	User_char->Render(m_pImmediateContext.Get());
+
+	BG->SetMatrix(nullptr, &Main_cam->m_View_matrix, &Main_cam->m_Proj_matrix);
+	BG->Render();
+
+	return true;
+}
+bool Sample::Release()
+{
+	
+	for (auto fbx : m_fbx_list)
+	{
+		fbx->Release();
+	}
+
+	//for (auto npc : m_NpcList)
+	//{
+	//	npc->Release();
+	//	delete npc;
+	//}
+
+	if (Main_cam) {
+		Main_cam->Release();
+		delete Main_cam;
+	}
+
+	User_char->Release();
+	delete User_char;
+
+	BG->Release();
+	delete BG;
+
+	return true;
+}
+
+
+int APIENTRY wWinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+
+	Sample demo;
+	demo.SetWindow(hinstance, L"LibraryTest",800,600);
+
+
+	demo.Run();
+
+	return 1;
+}
