bf40a0e168fd0e9477560fcce3df75436ad79846
KGCA4100 <KGCA4100@DESKTOP-AEAGHOI>
2022-11-14 18:39:56 +0900

    Height Map

--- include/Map.h
+++ include/Map.h
@@ -5,13 +5,21 @@
 class Map : public Base_object
 {
 public:
-	DWORD        rows_num;
-	DWORD        cols_num;
-	Quad_tree	 Map_Qtree;
+	DWORD					faces_num;
+	DWORD					rows_num;
+	DWORD					cols_num;
+	Quad_tree				Map_Qtree;
+public:		// height map
+	std::vector<float>		m_Height_list;
+	float					max_Height = 100.0f;
+
 public:
 	bool Build(ID3D11Device* pd3dDevice , UINT width, UINT height, Camera* cam);
 	bool Create_Qtree(ID3D11Device* pd3dDevice, Camera* cam);
 	bool Render() override;
 
+	bool Load_height_map(ID3D11Device* pd3dDevice, ID3D11DeviceContext* pContext, W_STR Texture);
+	void Gen_vertex_n();
+	Vector Calc_face_normal(UINT i0, UINT i1, UINT i2);
 };
 
--- okaka94/KJMCore/Map.cpp
+++ okaka94/KJMCore/Map.cpp
@@ -11,8 +11,8 @@ bool Map::Build(ID3D11Device* pd3dDevice, UINT width, UINT height, Camera* cam) 
 
 	for (int row = 0; row < height; row++) {	
 		for (int col = 0; col < width; col++) {
-			m_VertexList[row * width + col].p = { (float)(col - half_width) * cell_size, 
-			0.0f ,
+			m_VertexList[row * width + col].p = { (float)(col - half_width) * cell_size,
+			m_Height_list[row * width + col] * max_Height,
 			(float)(half_height - row) * cell_size };  // z 좌표는 - 연산 (좌상단 정점 -1,0,1)
 			m_VertexList[row * width + col].c = { 1.0f,1.0f,1.0f,1.0f };
 			m_VertexList[row * width + col].t = { (float)col / (width - 1)*10, (float)row / (height - 1)*10 };			// uv좌표계는 0 ~ 1로 정규화
@@ -20,6 +20,7 @@ bool Map::Build(ID3D11Device* pd3dDevice, UINT width, UINT height, Camera* cam) 
 	}
 
 	m_IndexList.resize((width - 1) * (height - 1) * 2 * 3);		// 셀 하나에 삼각형 페이스 2개 * 페이스 1개에 인덱스 3개
+	faces_num = m_IndexList.size() / 3;
 	UINT index = 0;
 	for (int row = 0; row < height - 1; row++) {				// width가 9면 7정점에서 8,16,17까지 한번에 인덱싱하므로 row < height - 1 이런식으로 만들면 됨
 		for (int col = 0; col < width - 1; col++) {
@@ -37,7 +38,7 @@ bool Map::Build(ID3D11Device* pd3dDevice, UINT width, UINT height, Camera* cam) 
 		}
 	}
 
-	//Create_Qtree(pd3dDevice,cam);
+	//Generate Normal
 
 	return true;
 }
@@ -59,4 +60,92 @@ bool Map::Render() {
 		m_pImmediateContext->DrawIndexed(node->m_Index_list.size(), 0, 0);
 	}
 	return true;
+}
+
+bool Map::Load_height_map(ID3D11Device* pd3dDevice, ID3D11DeviceContext* pContext, W_STR Texture) {
+
+	ComPtr<ID3D11Resource> texture;
+	HRESULT hr = DirectX::CreateWICTextureFromFileEx(pd3dDevice, Texture.c_str(), 0, 
+		D3D11_USAGE_STAGING, 0,										// Staging 
+		D3D11_CPU_ACCESS_READ | D3D11_CPU_ACCESS_WRITE, 0, 
+		WIC_LOADER_DEFAULT, texture.GetAddressOf(), nullptr);
+
+	ID3D11Texture2D* tex_2d = NULL;
+	if (FAILED(texture->QueryInterface(__uuidof(ID3D11Texture2D), (LPVOID*)&tex_2d))) {			// texture Resoucre -> tex2D
+		return false;
+	}
+
+	D3D11_TEXTURE2D_DESC td;
+	tex_2d->GetDesc(&td);
+
+	m_Height_list.resize(td.Height * td.Width);
+
+	D3D11_MAPPED_SUBRESOURCE	Mapped_face;		// D3D11_MAPPED_SUBRESOURCE : Provides access to subresource data.
+	if (SUCCEEDED(pContext->Map(tex_2d, D3D11CalcSubresource(0, 0, 1), D3D11_MAP_READ, 0, &Mapped_face))) {
+		UCHAR* Texels = (UCHAR*)Mapped_face.pData;
+		for (UINT row = 0; row < td.Height; row++) {
+			UINT row_start = row * Mapped_face.RowPitch;
+			for (UINT col = 0; col < td.Width; col++) {
+				UINT col_start = col * 4;
+				UINT Green = Texels[row_start + col_start + 1];
+				m_Height_list[row * td.Width + col] = (float)Green / 255.0f;		// 0 ~ 1 Range
+			}
+		}
+		pContext->Unmap(tex_2d, D3D11CalcSubresource(0, 0, 1));
+	}
+
+	rows_num = td.Height;
+	cols_num = td.Width;
+
+	tex_2d->Release();		// uuidof -> release()
+
+	return true;
+}
+
+void Map::Gen_vertex_n() {
+
+	std::vector<Vector> face_normals;
+	face_normals.resize(faces_num);
+	UINT face_idx = 0;
+	for (UINT i = 0; i < m_IndexList.size(); i += 3) {
+		UINT i0 = m_IndexList[i+0];
+		UINT i1 = m_IndexList[i+1];
+		UINT i2 = m_IndexList[i+2];
+		face_normals[face_idx++] = Calc_face_normal(i0,i1,i2);
+	}
+	struct Vertex_info {
+		std::vector<UINT>	adjacent_face_idx;
+		Vector				vertex_normal;
+	};
+
+	std::vector<Vertex_info> Vertex_info_list;
+	Vertex_info_list.resize(rows_num* cols_num);
+	for (UINT face = 0; face < faces_num; face++) {
+		for (UINT i = 0; i < 3; i++) {
+			UINT i0 = m_IndexList[face * 3 + i];						// i0 = 해당 인덱스의 정점 위치가 들어있음
+			Vertex_info_list[i0].adjacent_face_idx.push_back(face);		// i0번 정점에 인접한 face의 번호를 push_back
+		}
+
+	}
+	for (UINT vertex_num = 0; vertex_num < Vertex_info_list.size(); vertex_num++) {
+		for (UINT i = 0; i < Vertex_info_list[vertex_num].adjacent_face_idx.size(); i++) {
+			Vertex_info_list[vertex_num].vertex_normal += face_normals[Vertex_info_list[vertex_num].adjacent_face_idx[i]];
+		}
+		Vertex_info_list[vertex_num].vertex_normal.Normalize_vector();
+		m_VertexList[vertex_num].n = Vertex_info_list[vertex_num].vertex_normal;
+	}
+
+}
+
+Vector Map::Calc_face_normal(UINT i0, UINT i1, UINT i2) {
+
+	Vector face_normal;
+
+	Vector E0 = m_VertexList[i1].p - m_VertexList[i0].p;
+	Vector E1 = m_VertexList[i2].p - m_VertexList[i0].p;
+	
+	face_normal = E0 ^ E1;
+	face_normal.Normalize_vector();
+		
+	return face_normal;
 }
