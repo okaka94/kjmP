fc4689163c1b7fbe58bab8678cd32b832711ee1c
KGCA4100 <KGCA4100@DESKTOP-AEAGHOI>
2023-01-17 18:55:04 +0900

    fileIO

---
+++ okaka94/FILEIO/FILEIO.cpp
@@ -0,0 +1,20 @@
+﻿#include <windows.h>
+#include <iostream>
+
+wchar_t* g_fileBuffer = 0;
+
+DWORD Load(std::wstring file) {
+    HANDLE readFile = CreateFile(file.c_str(), GENERIC_READ, 0 , NULL , OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)
+}
+
+
+int main()
+{
+    std::wstring read = L"Text.iso";
+    std::wstring write = L"Copy.zip";
+
+    DWORD fileSize = Load(read);
+    Copy(write, fileSize);
+
+    std::cout << "Hello World!\n";
+}
