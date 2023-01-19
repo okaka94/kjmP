264db9fbde6be9629c996522024048253840f59c
KGCA4100 <KGCA4100@DESKTOP-AEAGHOI>
2023-01-19 11:21:05 +0900

    FILE IO _sync

--- okaka94/FILEIO/FILEIO.cpp
+++ okaka94/FILEIO/FILEIO.cpp
@@ -4,14 +4,42 @@
 wchar_t* g_fileBuffer = 0;
 
 DWORD Load(std::wstring file) {
-    HANDLE readFile = CreateFile(file.c_str(), GENERIC_READ, 0 , NULL , OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)
+    HANDLE readFile = CreateFile(file.c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    DWORD read = 0;
+    LARGE_INTEGER fileSize;
+
+    if (readFile != INVALID_HANDLE_VALUE) {
+        ::GetFileSizeEx(readFile, &fileSize);
+        g_fileBuffer = new wchar_t[fileSize.LowPart];
+        BOOL ret = ReadFile(readFile, g_fileBuffer, fileSize.QuadPart, &read, NULL);
+
+        if (ret == TRUE) {
+
+        }
+        CloseHandle(readFile);
+    }
+    return read;
+}
+
+DWORD Copy(std::wstring file, DWORD fileSize) {
+    HANDLE writeFile = CreateFile(file.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    DWORD write = 0;
+    
+    if (writeFile != INVALID_HANDLE_VALUE) {
+        BOOL ret = ::WriteFile(writeFile, g_fileBuffer, fileSize, &write, NULL);
+        if (ret == TRUE) {
+
+        }
+        CloseHandle(writeFile);
+    }
+    return write;
 }
 
 
 int main()
 {
-    std::wstring read = L"Text.iso";
-    std::wstring write = L"Copy.zip";
+    std::wstring read = L"Text.txt";
+    std::wstring write = L"Copy.txt";
 
     DWORD fileSize = Load(read);
     Copy(write, fileSize);
