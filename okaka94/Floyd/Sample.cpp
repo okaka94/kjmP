e5b08241b35ea685a5756ac6e7953209cdc6ca51
종민 김 <fpcmrj@knou.ac.kr>
2023-01-29 17:03:48 +0900

    Floyd _ begin

---
+++ okaka94/Floyd/Sample.cpp
@@ -0,0 +1,25 @@
+﻿#include "AStar.h"
+
+int main()
+{
+    AStar::MapGen map;
+    map.SetWorldSize({ 8, 5 });
+    //                x, y
+    map.AddObstacle({ 1, 1 });
+    map.AddObstacle({ 1, 2 });
+    map.AddObstacle({ 1, 3 });
+    map.AddObstacle({ 4, 0 });
+    map.AddObstacle({ 4, 1 });
+    map.AddObstacle({ 4, 2 });
+    map.AddObstacle({ 4, 3 });
+    map.AddObstacle({ 2, 3 });
+    map.AddObstacle({ 3, 3 });
+
+    auto path = map.FindPath({ 2,2 }, { 6,2 });
+
+    for (auto& index : path)
+    {
+        std::cout << index.x << " " << index.y << "\n";
+    }
+    std::cout << "End\n";
+}
